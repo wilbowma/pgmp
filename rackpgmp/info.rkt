ff3b7451487f2372a08665693e3542f5e042200b
William J. Bowman <wjb@williamjbowman.com>
2015-02-10 02:01:05 -0500

    Updated dependencies

--- rackpgmp/info.rkt
+++ rackpgmp/info.rkt
@@ -2,6 +2,10 @@
 (define collection 'multi)
 (define setup-collects '("pgmp" "perflinty"))
 (define deps '("base"
+               "racket-doc"
+               "errortrace-doc"
+               "scribble-lib"
+               "sandbox-lib"
                "errortrace-lib"
                "rackunit-lib"
                "r6rs-lib"))
